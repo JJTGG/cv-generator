6941303559ebfd80613709720537113f788768ab
JAYJAYTGG <abdjabbarbakre@gmail.com>
2026-09-24 14:24:13 +0100

    refactor: add CV template registry

--- components/preview/CVPreview.tsx
+++ components/preview/CVPreview.tsx
@@ -1,15 +1,25 @@
 import type { CVDocument } from "@/lib/document/types";
-import { ProfessionalTemplate } from "./templates/ProfessionalTemplate";
+import { getTemplate } from "@/lib/templates/registry";
 
 type CVPreviewProps = {
   document: CVDocument;
 };
 
 export function CVPreview({ document }: CVPreviewProps) {
+  const template =
+    getTemplate(document.settings.templateId) ??
+    getTemplate("professional");
+
+  if (!template) {
+    return null;
+  }
+
+  const Template = template.component;
+
   return (
     <section className="cv-print-root min-w-0 overflow-x-auto bg-[#deded9] p-5 lg:p-10 print:bg-white print:p-0">
       <div className="cv-print-page mx-auto w-fit">
-        <ProfessionalTemplate document={document} />
+        <Template document={document} />
       </div>
     </section>
   );
